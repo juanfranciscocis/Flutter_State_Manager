1daf35531f892a3573395a6ca9a385d17973eea8
Juan Cisneros <juanfrancistm2011@icloud.com>
2023-05-17 11:50:19 -0500

    PAGE 2 FINISHED AND ADDED THE USER MODEL

--- singleton_state_manager/lib/pages/page2.dart
+++ singleton_state_manager/lib/pages/page2.dart
@@ -1,21 +1,60 @@
 import 'package:flutter/material.dart';
 
 class Page2 extends StatelessWidget {
+
   const Page2({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Color buttonColor = Theme.of(context).colorScheme.primary;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Page 2'),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
-      body: Center(
-      child: Text(
-        'Page 2',
-        style: Theme.of(context).textTheme.headline1,
-      ),
-    )
-    );
+        appBar: AppBar(
+          title: const Text('Page 2'),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          elevation: 50,
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  //add the color but make it more transparent
+                  color: buttonColor.withAlpha(230),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  height: 50,
+                  child: Text("Establecer Usuario", style: TextStyle(color: Colors.white),),
+                  onPressed: () {
+
+                }),
+
+                const SizedBox(height: 20),
+
+                MaterialButton(
+                  color: buttonColor.withAlpha(230),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  height: 50,
+                  child: Text("Cambiar Edad", style: TextStyle(color: Colors.white),),
+                  onPressed: () {
+
+                }),
+
+                const SizedBox(height: 20),
+
+                MaterialButton(
+                  color: buttonColor.withAlpha(230),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  height: 50,
+                  child: Text("Añadir Profesion", style: TextStyle(color: Colors.white),),
+                  onPressed: () {
+
+                }),
+
+                
+
+                ],
+        )));
   }
 }
